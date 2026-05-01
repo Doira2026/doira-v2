a9c97342cd2085db63c677290023db9d180282b8
Doira2026 <sapaev838591@gmail.com>
2026-05-01 23:30:45 +0500

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,14 @@
+// Doira Chat Service Worker
+const CACHE_NAME = 'doira-chat-v1';
+
+self.addEventListener('install', (event) => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(clients.claim());
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(fetch(event.request));
+});
